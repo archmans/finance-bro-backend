3769dcaff7f24fe55ca073d52b08d97fb616a2be
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-12-10 20:33:25 +0700

    refactor: env value

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -4,7 +4,9 @@ dotenv.config();
 
 const environment = {
     PORT: process.env.PORT || 5000,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET,
+    PROJECT_ID: process.env.PROJECT_ID,
+    DATABASE_ID: process.env.DATABASE_ID,
 };
 
 export default environment;

5b723dff56e0003af11f9af8b27cdb85d0dae743
Azmi Hasna Zahrani <88821341+goodgirlwannabe@users.noreply.github.com>
2024-12-12 00:23:13 +0700

    feat: add api documentation swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,225 @@ import calculateRoutes from './calculator.routes';
 import stockRoutes from './stock.routes';
 import { authenticateToken } from '../middleware/middleware';
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Firebase
+ *     description: Endpoints for Firebase operations
+ */
+
+/**
+ * @swagger
+ * /firebase:
+ *   post:
+ *     summary: Add user to Firebase
+ *     tags: [Firebase]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The email address associated with the Firebase operation.
+ *                 example: user@example.com
+ *     responses:
+ *       200:
+ *         description: Firebase operation successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Firebase operation completed successfully.
+ *       500:
+ *         description: Internal server error
+ *   delete:
+ *     summary: Delete user from Firebase
+ *     tags: [Firebase]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Firebase operation successful.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User deleted
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /tracker:
+ *   get:
+ *     summary: Get profile data
+ *     tags: [Tracker]
+ *     description: Get user-specific tracking data. Requires authentication.
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved the tracking data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   example: salman
+ *                 age:
+ *                   type: number
+ *                   example: 25
+ *                 retireAge:
+ *                   type: number
+ *                   example: 65
+ *                 retirePeriod:
+ *                   type: number
+ *                   example: 20
+ *       500:
+ *         description: Internal server error
+ *   post:
+ *     summary: Tracker routes for data tracking
+ *     tags: [Tracker]
+ *     description: Manage user-specific tracking data. Requires authentication.
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the tracking data.
+ *                 example: salman
+ *               age:
+ *                 type: number
+ *                 description: The age of the user.
+ *                 example: 25
+ *               retireAge:
+ *                 type: number
+ *                 description: The age at which the user plans to retire.
+ *                 example: 65
+ *               retirePeriod:
+ *                 type: number
+ *                 description: The number of years the user plans to be retired.
+ *                 example: 20
+ *               monthlyExpenses:
+ *                 type: number
+ *                 description: The user's monthly expenses.
+ *                 example: 2000000
+ *     responses:
+ *       200:
+ *         description: Successfully tracked the data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Tracking data saved successfully."
+ *       500:
+ *         description: Internal server error
+ *   put:
+ *     summary: Update profile data
+ *     tags: [Tracker]
+ *     description: Update user-specific tracking data. Requires authentication.
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the tracking data.
+ *                 example: salman
+ *               age:
+ *                 type: number
+ *                 description: The age of the user.
+ *                 example: 25
+ *               retireAge:
+ *                 type: number
+ *                 description: The age at which the user plans to retire.
+ *                 example: 60
+ *               retirePeriod:
+ *                 type: number
+ *                 description: The number of years the user plans to be retired.
+ *                 example: 25
+ *               monthlyExpenses:
+ *                 type: number
+ *                 description: The user's monthly expenses.
+ *                 example: 2000000
+ *     responses:
+ *       200:
+ *         description: Successfully updated the data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Tracking data updated successfully."
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /calculator:
+ *   get:
+ *     summary: Financial calculator routes
+ *     tags: [Calculator]
+ *     security:
+ *       - bearerAuth: []
+ *     description: Perform financial calculations. Requires authentication.
+ *     responses:
+ *       200:
+ *         description: Successfully calculated the financial data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: integer
+ *                   description: HTTP status code.
+ *                   example: 200
+ *                 savingNeedPerMonth:
+ *                   type: number
+ *                   nullable: true
+ *                   description: Monthly saving needed for the goal. Can be null if not applicable.
+ *                   example: null
+ *                 savingGoal:
+ *                   type: number
+ *                   nullable: true
+ *                   description: Total saving goal amount. Can be null if not applicable.
+ *                   example: null
+ *       500:
+ *         description: Internal server error
+ */
+
 const router = Router();
 
 router.use('/auth', userRoutes);
--- src/routes/tracker.routes.ts
+++ src/routes/tracker.routes.ts
@@ -10,6 +10,197 @@ import {
     updateSavingsInvest,
     deleteSavings
 } from '../module/savingTracker/tracker.controller';
+
+/**
+ * @swagger
+ * tags:
+ *   name: SavingTracker
+ *   description: API for managing savings and profiles
+ */
+
+/**
+ * @swagger
+ * /tracker/savings:
+ *   get:
+ *     summary: Get all savings records
+ *     tags: [SavingTracker]
+ *     description: Retrieve all saving records for the user.
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of savings retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 savings:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       name:
+ *                         type: string
+ *                         example: "Emergency Fund"
+ *                       amount:
+ *                         type: number
+ *                         example: 1000
+ *                       type:
+ *                         type: string
+ *                         example: "Investment"
+ *       401:
+ *         description: Unauthorized access.
+ *   post:
+ *     summary: Add a new saving record
+ *     tags: [SavingTracker]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Emergency Fund"
+ *               amount:
+ *                 type: number
+ *                 example: 1000
+ *               type:
+ *                 type: string
+ *                 example: "Investment"
+ *     responses:
+ *       201:
+ *         description: Saving added successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                  type: string
+ *                  example: "Saving added"
+ *       401:
+ *         description: Unauthorized access.
+ *   put:
+ *     summary: Update an existing saving record
+ *     tags: [SavingTracker]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Emergency Fund"
+ *               amount:
+ *                 type: number
+ *                 example: 1000
+ *               type:
+ *                 type: string
+ *                 example: "Saving"
+ *     responses:
+ *       200:
+ *         description: Saving updated successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                  type: string
+ *                  example: "Savings updated"
+ *       401:
+ *         description: Unauthorized access.
+ *   delete:
+ *     summary: Delete a saving record
+ *     tags: [SavingTracker]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Emergency Fund"
+ *     responses:
+ *       200:
+ *         description: Saving deleted successfully.
+ *       401:
+ *         description: Unauthorized access.
+ */
+
+/**
+ * @swagger
+ * /tracker/savingsname:
+ *   get:
+ *     summary: Get all saving names
+ *     tags: [SavingTracker]
+ *     responses:
+ *       200:
+ *         description: List of saving names retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: string
+ *               example: ["Emergency Fund", "Vacation Savings"]
+ *       404:
+ *         description: No saving names found.
+ */
+
+/**
+ * @swagger
+ * /tracker/savingsinvest:
+ *  put:
+ *     summary: Update an existing saving record
+ *     tags: [SavingTracker]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Emergency Fund"
+ *               amount:
+ *                 type: number
+ *                 example: 1000
+ *               type:
+ *                 type: string
+ *                 example: "Investment"
+ *     responses:
+ *       200:
+ *         description: Saving updated successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                  type: string
+ *                  example: "Savings invest updated"
+ *       401:
+ *         description: Unauthorized access.
+ *  
+ */
+
 const router = Router();
 router.post('/', createProfile);
 router.get('/', getProfile);
